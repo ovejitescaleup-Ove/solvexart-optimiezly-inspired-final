fd71af7ce62dd3055754e22442f3e9a77327e0ff
OVEJITE KUMAR VHOWMICK <ovejite.scaleup@gmail.com>
2026-08-31 02:02:13 +0600

    Delete components directory

--- components/SiteShell.tsx
+++
@@ -1,2 +0,0 @@
-import Navbar from './Navbar'; import Footer from './Footer';
-export default function SiteShell({children}:{children:React.ReactNode}){return <><Navbar/><main>{children}</main><Footer/></>}
